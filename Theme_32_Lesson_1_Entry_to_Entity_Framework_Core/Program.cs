ed165837538dc6dea67387db2ebddbbe320eff3c
User <User@DESKTOP-I0GP3BT>
2024-06-03 16:48:45 +0300

    Добавьте файлы проекта.

---
+++ Theme_32_Lesson_1_Entry_to_Entity_Framework_Core/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Theme_32_Lesson_1_Entry_to_Entity_Framework_Core
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
