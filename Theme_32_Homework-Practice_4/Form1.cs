4d277de27852c0adac77278a266425caf68691ff
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-08-04 19:07:30 +0300

    все методы по фильтрации пассажиров в зависимости от условий успешно реализованы

--- Theme_32_Homework-Practice_4/Form1.cs
+++ Theme_32_Homework-Practice_4/Form1.cs
@@ -9,10 +9,8 @@ namespace Theme_32_Homework_Practice_4
         public Form1()
         {
             InitializeComponent();
-
-            UploadData();
-
             TimeCount();
+            UploadData();
         }
 
 
@@ -27,8 +25,7 @@ namespace Theme_32_Homework_Practice_4
             }
         }
 
-
-
+        
         // Предположительно асинхронный(нет) метод, считающий время работы приложения 
         public void TimeCount()
         {
@@ -45,8 +42,21 @@ namespace Theme_32_Homework_Practice_4
                 }
             });
         }
-
+        /// <summary>
+        /// Кнопка по выводу выживших пассажиров
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void survivedBTN_Click(object sender, EventArgs e)
+        {
+            UploadData();
+            ShowSurvivedPassengers();
+        }
+
+        /// <summary>
+        /// Метод по поиску выживших пассажиров
+        /// </summary>
+        public void ShowSurvivedPassengers()
         {
             using (ApplicationContext context = new ApplicationContext())
             {
@@ -55,39 +65,81 @@ namespace Theme_32_Homework_Practice_4
             }
         }
 
+        /// <summary>
+        /// Кнопка по выводу взрослых пассажиров
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void adultBTN_Click(object sender, EventArgs e)
         {
-            using (ApplicationContext context = new ApplicationContext())
-            {
-
-                passengers = context.Passengers.ToList();
-
-                for (int i = 0; i < passengers.Count; i++)
-                {
-                    if (passengers[i].Age.Contains('.'))
-                    {
-                        passengers[i].Age = passengers[i].Age.Replace('.', ',');
-                    }
-
-                    if (passengers[i].Age + "1" == "1") 
-                    {
-                        passengers[i].Age = "10";
-
-                    }
-
-                    if (double.Parse(passengers[i].Age) < 18)
-                    {
-                        passengers[i].Age = "0";
-                    }
-                }
-
-                passengersDGV.DataSource = passengers;
-            }
+            UploadData();
+            ShowAdultPassengers();
         }
 
+        /// <summary>
+        /// Метод по поиску взрослых пассажиров
+        /// </summary>
+        public void ShowAdultPassengers()
+        {
+            List<Passenger> adults = new List<Passenger>();
+
+            for (int i = 0; i < passengers.Count; i++)
+            {
+                if (passengers[i].Age.Contains('.'))
+                {
+                    passengers[i].Age = passengers[i].Age.Replace('.', ',');
+                }
+
+                if (passengers[i].Age + "1" == "1")
+                {
+                    passengers[i].Age = "10";
+
+                }
+
+                if (double.Parse(passengers[i].Age) < 18)
+                {
+                    passengers[i].Age = "0";
+                }
+            }
+
+            for (int i = 0; i < passengers.Count; i++)
+            {
+                if (double.Parse(passengers[i].Age) != 0)
+                {
+                    adults.Add(passengers[i]);
+                }
+            }
+            passengersDGV.DataSource = adults;
+        }
+
+
+        /// <summary>
+        /// Кнопка по выводу пассажиров третьего класса
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void thirdClassBTN_Click(object sender, EventArgs e)
         {
+            UploadData();
+            ShowThirdClassPassengers();
+        }
 
+
+        /// <summary>
+        /// Метод по поиску пассажиров третьего класса
+        /// </summary>
+        public void ShowThirdClassPassengers()
+        {
+            List<Passenger> thirdClassList = new List<Passenger>();
+
+            foreach (var passenger in passengers)
+            {
+                if (passenger.Pclass == 3)
+                {
+                    thirdClassList.Add(passenger);
+                }
+            }
+            passengersDGV.DataSource = thirdClassList;
         }
     }
 }
