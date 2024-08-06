3032245b7bf953b6c186feeac8b4473064c9e9da
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-08-06 21:24:08 +0300

    знакомство с логами

---
+++ Theme_34_Lesson_4_Logging/DataBaseContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theme_34_Lesson_4_Logging
+{
+    public class DataBaseContext : DbContext
+    {
+        private StreamWriter _logWriter = new StreamWriter("loggingFile.log", true);
+        public DbSet<User> Users { get; set; } = null!;
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            _logWriter.Dispose();
+
+        }
+
+        public DataBaseContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=192.168.0.13;database=WorkDatabase;user=student-cifra;password=000000", new MySqlServerVersion(new Version(8, 0, 25)));
+            optionsBuilder.LogTo(_logWriter.WriteLine, LogLevel.Debug);
+        }
+    }
+}
---
+++ Theme_34_Lesson_4_Logging/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Theme_34_Lesson_4_Logging
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            using (DataBaseContext db = new DataBaseContext())
+            {
+                List<User> users = new List<User>()
+                {
+                    new User("Валерий", "Valery@mail.ru", "000000"),
+                    new User("Елена", "Elena@gmail.com", "123456"),
+                    new User("Иван", "Ivan@example.com", "qwerty"),
+                    new User("Анна", "Anna@mail.ru", "password123")
+                };
+
+                db.Users.AddRange(users);
+                db.SaveChanges();
+            }
+            Console.WriteLine("Успешно");
+
+        }
+    }
+}
