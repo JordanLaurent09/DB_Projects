f92a24b0f64598251241ce66500406855e278d55
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2024-08-08 19:09:10 +0300

    Логи затирают один другой. Общий смысл понятен, нюансы пока не решены

--- Theme_34_Homework_4/DataBaseContext.cs
+++ Theme_34_Homework_4/DataBaseContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +10,42 @@ namespace Theme_34_Homework_4
 {
     public class DataBaseContext : DbContext
     {
+        private StreamWriter LogDebugWriter = new StreamWriter("LoggingDebug.log", true);
+        private StreamWriter LogErrorWriter = new StreamWriter("LoggingErrors.log", true);
         public DbSet<Product> Products { get; set; } = null!;
-        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options) 
+        public DataBaseContext()
         {
             Database.EnsureDeleted();
             Database.EnsureCreated();
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var builder = new ConfigurationBuilder();
+
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("internetShopConfig.json");
+
+            var config = builder.Build();
+
+            var connectionString = config.GetConnectionString("DefaultConnection");
+
+
+            var version = new MySqlServerVersion(new Version(8, 0, 25));
+
+            optionsBuilder.UseMySql(connectionString, version);
+
+            optionsBuilder.LogTo(LogDebugWriter.WriteLine, Microsoft.Extensions.Logging.LogLevel.Debug);
+            optionsBuilder.LogTo(LogErrorWriter.WriteLine, Microsoft.Extensions.Logging.LogLevel.Error);
+
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            LogDebugWriter.Dispose();
+            LogErrorWriter.Dispose();
         }
     }
 }
